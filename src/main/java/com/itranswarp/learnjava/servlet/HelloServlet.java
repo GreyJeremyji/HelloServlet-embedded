c6cfe34f1b7640963dc45ced169e5d6c11f48016
jiyaozong <1048442010@qq.com>
2022-06-17 01:09:26 +0800

    20220617

--- src/main/java/com/itranswarp/learnjava/servlet/HelloServlet.java
+++ src/main/java/com/itranswarp/learnjava/servlet/HelloServlet.java
@@ -9,12 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/")
+@WebServlet(urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
+
+
+
         String name = req.getParameter("name");
         if (name == null){
             name = "world";
